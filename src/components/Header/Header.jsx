8779a6ef9e4f894a1c79cde59b1f5772b5a5b925
Aniket Singh <thor98571@gmail.com>
2024-02-20 10:33:58 +0530

    added timer project for showing memo and callback

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,37 @@
 import React,{useState} from "react"
 import logo from '../../assets/logo.png'
 import box from '../../assets/box.png'
+import { useSDK } from "@metamask/sdk-react";
 export default function Header(){
     const [activeLink,setActive]=useState("Evolution")
+    const { sdk, connected, connecting, provider, chainId } = useSDK();
+    const connect=async()=>{
+        try {
+            const accounts = await sdk?.connect();
+            console.log(accounts)
+        } catch (err) {
+            console.warn("failed to connect..", err);
+        }
+    }
+    const sign=async()=>{
+        // sdk.connectAndSign
+        // const signResult = await sdk?.connectAndSign({
+        //     msg: "Connect + Sign message",
+        // });
+        // console.log(signResult)
+        try {
+            const from = "0xB90657d0af39EdF73bf5553da9BDF2f68Ae16198";
+            const msg = `0x${Buffer.from("0xB90657d0af39EdF73bf5553da9BDF2f68Ae16198", "utf8").toString("hex")}`;
+            const sign = await ethereum.request({
+                method: "personal_sign",
+                params: [msg, from],
+            });
+            console.log(sign)
+        } catch (err) {
+            console.error(err);
+            
+        }
+    }
     return (
         <div> 
             <div class="w-page-width mx-auto m-0 flex justify-between items-center pt-5 pb-5">
@@ -12,11 +41,16 @@ export default function Header(){
                 <div class="flex items-center gap-10">
                     <p class={`text-${activeLink==="Evolution"?"active":"white"} text-xl cursor-pointer font-mono-regular`} onClick={()=>setActive("Evolution")}>Evolution</p>
                     <p className={`text-${activeLink==="Mint"?"active":"white"} text-xl cursor-pointer font-mono-regular`} onClick={()=>setActive("Mint")}>Mint</p>
+                    
                 </div>
                 <div>
-                    <button type="button" className="w-24 relative">
+                    <button type="button" className="w-24 relative" onClick={connect}>
                         <img className="w-full" src={box} alt="box"/>
                          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-mono-regular"> Profile</p>
+                    </button>
+                    <button type="button" className="w-24 relative" onClick={sign}>
+                        <img className="w-full" src={box} alt="box"/>
+                         <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-mono-regular"> Sign</p>
                     </button>
                     <button type="button" className="hidden">
                         <div className="bg-white h-3 w-3"></div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { MetaMaskProvider } from "@metamask/sdk-react";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <MetaMaskProvider
+            debug={false}
+            sdkOptions={{
+                dappMetadata: {
+                    name: "ZCats",
+                    url: window.location.href,
+                },
+                // Other options
+            }}
+        >
+       <App />
+    </MetaMaskProvider>
   </React.StrictMode>,
 )
